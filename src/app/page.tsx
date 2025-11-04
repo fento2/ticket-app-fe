1b0b2592d256b12ef9f23a0914115198097179b0
fento2 <ftonrate91@gmail.com>
2025-11-05 02:27:22 +0700

    embla

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Category from "@/components/shared/Category";
 import DivParentPage from "@/components/shared/DivParentPage";
+import Embla from "@/components/shared/Embla";
 import SlideEvent from "@/components/shared/SlideEvent";
 import SwipeEvent from "@/components/shared/SwipeEvent";
 import TopEvent from "@/components/shared/TopEvent";
@@ -17,6 +18,8 @@ export default function Home() {
 
       {/* top event */}
       <TopEvent />
+
+      <Embla />
 
       <DivParentPage className="my-8 space-y-12">
 
